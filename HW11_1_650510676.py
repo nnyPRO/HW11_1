c2d8b0bf36f65d2ec129fa5eaea0a1018c606281
nnyPRO <nanmesanee.17@gmail.com>
2023-09-21 16:59:55 +0700

    finish

--- HW11_1_650510676.py
+++ HW11_1_650510676.py
@@ -35,17 +35,18 @@ def q02():
 
 def q03():
     print("Q03")
+    print(np.__version__)
     result = sal.info()   # edit this
 
-    print(result)   # Dont change this
+    # print(result)   # Dont change this
 
 # 4) ** What is the average BasePay ?
 
 
 def q04():
     print("Q04")
-    rows = len(sal.axes[0])
-    result = (sal['BasePay'].sum())/rows   # edit this
+    # rows = len(sal.axes[0])
+    result = (sal['BasePay'].mean())  # edit this
 
     print(result)   # Dont change this
 
@@ -79,7 +80,7 @@ def q07():
 def q08():
     print("Q08")
     
-    result = sal[sal['TotalPay'] == sal['TotalPay'].max()]
+    result = sal[sal['TotalPayBenefits'] == sal['TotalPayBenefits'].max()]
 
     print(result)   # Dont change this
     # print(pd.__version__)
@@ -113,29 +114,34 @@ def q11():
 # 12) ** What are the top 5 most common jobs? **
 def q12():
     print("Q12")
-    # result = sal.groupby(['JobTitle'])['JobTitle'].count().head()
-    result = sal['JobTitle'].value_counts().head() # edit this
-
+    result = sal.groupby(['JobTitle'])['JobTitle'].count().sort_values(ascending=[False]).head()
+    # result = sal['JobTitle'].value_counts().head() # edit this
+# 
     print(result)   # Dont change this
 
+
 # 13) ** How many Job Titles were represented by only one person in 2013? (e.g. Job Titles with only one occurence in 2013?) **
-
-
 def q13():
-    print("Q13")
-    year = sal.groupby(['Year','JobTitle'], group_keys=True).apply(lambda x: x)
-    # year = sal.groupby([sal.index.month, sal['Category'])['Amount'].sum()])
-    # year = sal.groupby(['Year'])
-    # a = year.sum()
+    # print("Q13")
+    # year = sal.groupby(['Year','JobTitle'], group_keys=True).apply(lambda x: x)
+
+    ids = pd.factorize(sal['Year'])[1]
+    n = 3 #เอา group ที่เป็นปี 2013
+    m = sal['Year'].isin(ids[2:n])
+    new_sal = sal.loc[m]
+    count = new_sal['JobTitle'].value_counts()
+    num = count_num(count)
     
-    # a = year.get_group('2013')
-    # title = ['JobTitle'].value_counts() 
-    # new = year.set_index('Year','JobTitle')
-    # year = sal.query('Year=="2013"')
-    b = year.get_group('2013')
-    c = year.loc['2013']
-    result = 5
-    print(b)
+    print(num)
+
+#ฟังก์ชันที่เอามานับจำนวน value ที่ต้องการ
+def count_num(c):
+    num = 0
+    for i in c:
+        if i == 1:
+            num+=1
+    return num
+
 
 
 # 14) ** How many people have the word Chief in their job title? (This is pretty tricky) **
@@ -143,16 +149,38 @@ def q14():
     print("Q14")
 
     result = None
+    # job = sal[sal['JobTitle']=='CHIEF']['Id']
+    job = sal['JobTitle']
+    num_chief = count_job(job)
+    # re = sal['EmployeeName'].value_counts()
 
-    print(result)   # Dont change this
+    print(num_chief)   # Dont change this
+
+#ฟังก์ชันที่เอามานับจำนวน value ที่ต้องการของฟังก์ชัน q14
+def count_job(j):
+    num_j = 0
+    for i in j:
+        # if "CHIEF" in i:
+        #     print(i)
+        #     num_j+=1
+        if "chief" in i.lower():
+            # print(i)
+            num_j+=1
+    return num_j
 
 
 # 15) ** Bonus: show correlation between length of the Job Title string and Salary? **
 def q15():
     print("Q15")
-    result = None   # edit this
-
+    # sal['t'] = sal['JobTitle'].apply(len)
+    # result = sal[['t','TotalPayBenefits' ]].corr()
+    # result = None   # edit this
+    sal['t'] = sal['JobTitle'].apply(len)
+    # corr_matrix = sal.corr(method ='pearson')#['TotalPayBenefits',sal['JobTitle'].apply(lambda x: len(x))]
+    result = sal[['t','TotalPayBenefits' ]].corr()
     print(result)   # Dont change this
+
+    
 
 
 if __name__ == '__main__':
@@ -168,6 +196,6 @@ if __name__ == '__main__':
     # q10()
     # q11()
     # q12()
-    q13()
+    # q13()
     # q14()
-    # q15()
+    q15()
